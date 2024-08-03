752fd295ef7b937f9ad5cd27c7a4782bcb22b35f
RENAN DE FREITAS COELHO <RENAN0606@DIGITALCOLLEGE.LOCAL>
2024-08-03 12:44:43 -0300

    continuando projeto

--- bootstrap-Project/script.js
+++ bootstrap-Project/script.js
@@ -1,19 +1,50 @@
-const tableBody=document.createElement("main");
+
 const htjsDiv=document.createElement("div");
 const htjsHeader=document.createElement("header");
 const htInput=document.createElement("h1")
 const takingElement=document.getElementById("produtcts-list-table");
+
+// criando tags html
 const tableRoll=document.createElement("tr");
+const tableBody=document.createElement("tbody");
 
 
 
-takingElement.appendChild(tableRoll);
+
+
+createProduts();
+
+tableBody.appendChild(tableRoll);
+takingElement.appendChild(tableBody);
 
 
 
-console.log(tableBody);
-console.log(htjsDiv);
-console.log(htjsHeader);
-console.log(takingElement);
+function createProduts() {
+    for(let j=0;j<2;j++)
+        {
+
+            const tableRoll=[j];
+            const tableRoll=document.createElement("tr");
+    
+    for(let i=0;i<6;i++){
+
+        let name=["Tenis","Nike","380","500","jo3ao","ju4lio"]
+    
+    
+        const tableTd=document.createElement("td");
+        tableTd.innerText=name[`${i}`];
+        tableRoll.appendChild(tableTd);
+        
+    }
+}
+
+}
+
+
+
+
+
+
+
 
 
